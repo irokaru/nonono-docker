eaf056b79c963039f095e062e4db432fb8002c3b
irokaru <karuta@nononotyaya.net>
2020-04-09 23:59:58 +0900

    add history page

---
+++ src/nonono/resources/js/pages/admin/History.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h2>管理者用履歴管理ページ</h2>
+
+    <div class="content">
+
+      <router-link to="/admin">管理者ページ</router-link> <i class="fas fa-chevron-right"></i> 管理者用履歴管理ページ
+
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
