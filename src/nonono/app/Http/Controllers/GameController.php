7ab0de681f2ff9783711fbf23d8de37cbb14fcaf
irokaru <karuta@nononotyaya.net>
2020-08-02 17:08:58 +0900

    add update logic

--- src/nonono/app/Http/Controllers/GameController.php
+++ src/nonono/app/Http/Controllers/GameController.php
@@ -6,8 +6,10 @@ use App\Models\Game;
 use App\Http\Resources\GameResource;
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
+
+use App\Lib\CommonUtil;
 
 class GameController extends Controller
 {
@@ -22,14 +24,26 @@ class GameController extends Controller
         'url'            => 'required|string|min:1|max:257',
     ];
 
+    protected static $_cache_expire_min = 60;
+
     public function __construct()
     {
-        $this->middleware('auth:api')->only(['indexAll', 'store']);
+        $this->middleware('auth:api')->only(['indexAll', 'store', 'update']);
     }
 
     public function index()
     {
-        return GameResource::collection(Game::getReleasedGames());
+        if (!CommonUtil::isTesting() && Cache::has(static::getCacheKey())) {
+            return Cache::get(static::getCacheKey(), );
+        }
+
+        $games = GameResource::collection(Game::getReleasedGames());
+
+        if (!CommonUtil::isTesting()) {
+            Cache::put(static::getCacheKey(), $games, static::$_cache_expire_min);
+        }
+
+        return $games;
     }
 
     public function indexAll()
@@ -63,10 +77,60 @@ class GameController extends Controller
             'url'            => $request->url,
         ]);
 
+        if (Cache::has(static::getCacheKey())) {
+            Cache::forget(static::getCacheKey());
+        }
+
+        return response()->json(['status' => 'success'], 200);
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), static::$_validate);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $game = Game::find($request->id);
+        if (!$game) {
+            return response()->json(['error' => 'not found.', 400]);
+        }
+
+        if (!CommonUtil::isTesting()) {  // テストのときは削除処理はしない
+            unlink(public_path() . '/' . $game->thumbnail_path);
+        }
+
+        $thumbnail     = $request->file('thumbnail');
+        $thumbnail_ext = $thumbnail->extension();
+
+        $thumbnail_save_dir = static::getThumbnailSaveDir();
+        $thumbnail_path     = static::getThumbnailPath($request->thumbnail_name, $thumbnail_ext);
+        if (!CommonUtil::isTesting()) {  // テストの時は出力しない
+            $thumbnail->move($thumbnail_save_dir, $request->thumbnail_name.'.'.$thumbnail_ext);
+        }
+
+        $game->title          = $request->title;
+        $game->release_date   = $request->release_date;
+        $game->release_flag   = $request->release_flag;
+        $game->thumbnail_path = $thumbnail_path;
+        $game->category       = $request->category;
+        $game->infomation     = $request->infomation;
+        $game->url            = $request->url;
+        $game->save();
+
+        if (Cache::has(static::getCacheKey())) {
+            Cache::forget(static::getCacheKey());
+        }
+
         return response()->json(['status' => 'success'], 200);
     }
 
     // ---------------------------------------------------------------------------
+
+    protected static function getCacheKey(): string
+    {
+        return \md5('gamelist');
+    }
 
     /**
      * ゲームのサムネイルを保存するパスを返す
