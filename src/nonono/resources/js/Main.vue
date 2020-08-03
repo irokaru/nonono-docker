eca94648c0ad038f0fd463835aa1a6e60bf6a9ee
irokaru <karuta@nononotyaya.net>
2020-08-03 22:01:15 +0900

    add footer

--- src/nonono/resources/js/Main.vue
+++ src/nonono/resources/js/Main.vue
@@ -4,21 +4,25 @@
   <common-nav/>
 
   <div class="main">
+
     <transition name="fade" mode="out-in">
       <router-view></router-view>
     </transition>
-    <MyFooter/>
+
+    <common-footer/>
   </div>
 
 </div>
 </template>
 
 <script>
-import CommonNav from './components/CommonNav';
+import CommonNav    from './components/CommonNav';
+import CommonFooter from './components/CommonFooter';
 
 export default {
   components: {
     CommonNav,
+    CommonFooter,
   },
 };
 </script>
