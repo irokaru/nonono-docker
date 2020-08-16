b9768235f6b43e52cee2e57b9d26c2e259d147c3
irokaru <karuta@nononotyaya.net>
2020-08-17 00:12:28 +0900

    separate category column

--- src/nonono/database/migrations/2020_08_16_230832_create_post_categories_table.php
+++ src/nonono/database/migrations/2020_08_16_230832_create_post_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostTable extends Migration
+class CreatePostCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreatePostTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('post_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->date('date');
-            $table->boolean('release_flag');
+            $table->integer('post_id');
+            $table->string('category');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreatePostTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post');
+        Schema::dropIfExists('post_category');
     }
 }
