a3332a5747f1a6fc851f5ff4a6b6a58d68fad625
irokaru <karuta@nononotyaya.net>
2020-12-19 16:04:21 +0900

    separate methods

---
+++ src/nonono/resources/js/lib/BlogUtil.js
@@ -0,0 +1,78 @@
+export default {
+  /**
+   * パスのアクセスが正しいか判断する
+   * @param {string} mode
+   * @param {string} key
+   * @param {string} page
+   * @returns {boolean}
+   */
+  validateRoute(mode, key, page) {
+    if (mode === '' && key === '' && page === '') {
+      return true;
+    }
+
+    // -------------------------------------------------------
+    // 記事リスト
+    if (mode && key === '' && page === '') {
+      const modeNumber = this.toNumber(mode);
+
+      if (Validator.isInteger(modeNumber) && Validator.minNumber(modeNumber, 1)) {
+        return true;
+      }
+    }
+
+    // -------------------------------------------------------
+    // 記事詳細＆カテゴリリスト
+    if (mode && key && page === '') {
+      const keyNumber = this.toNumber(key);
+
+      if (mode === 'post' && Validator.isInteger(keyNumber) && Validator.minNumber(keyNumber, 1)) {
+        return true;
+      }
+
+      if (mode === 'category' && Validator.maxLength(key, 30)) {
+        return true;
+      }
+    }
+
+    // -------------------------------------------------------
+    // カテゴリリスト
+    if (mode && key && page) {
+      const pageNumber = this.toNumber(page);
+
+      if (mode === 'category' && Validator.maxLength(key, 30) && Validator.isInteger(pageNumber) && Validator.minNumber(pageNumber, 1)) {
+        return true;
+      }
+    }
+
+    return false;
+  },
+
+  /**
+   * モードを基にコンポーネント名を返す
+   * @param {string} mode
+   * @returns {string}
+   */
+  mainComponentName(mode) {
+    const modes = {
+      post:     'BlogPostDetail',
+      category: 'BlogCategoryList',
+      _other:   'BlogPostList',
+    };
+
+    if (!Validator.hasKeyInObject(modes, mode)) {
+      return modes._other;
+    }
+
+    return modes[mode];
+  },
+
+  /**
+   * 数値化する
+   * @param {string} val
+   * @returns {number}
+   */
+  toNumber(val) {
+    return val.match(/^[0-9]+$/) ? Number(val) : -1;
+  },
+};
--- src/nonono/resources/js/pages/Blog.vue
+++ src/nonono/resources/js/pages/Blog.vue
@@ -15,13 +15,16 @@
 
 <script>
 import BlogSideBar from '../components/BlogSideBar';
-import Post        from '../model/Post';
+
+import BlogUtil from '../lib/BlogUtil';
+import Post     from '../model/Post';
 
 export default {
   data () {
     return {
-      latests:    Array(4).fill(Post.dummy(-1)),
-      categories: Array(4).fill(Post.category),
+      currentView: 'BlogPostList',
+      latests:     Array(4).fill(Post.dummy(-1)),
+      categories:  Array(4).fill(Post.category),
     }
   },
   methods: {
@@ -48,71 +51,12 @@ export default {
     getPage() {
       return this.$route.params.page || '';
     },
-
-    /**
-     * 数値化する
-     * @param {string} val
-     * @returns {number}
-     */
-    toNumber(val) {
-      return val.match(/^[0-9]+$/) ? Number(val) : -1;
-    },
-
-    /**
-     * パスのアクセスが正しいか判断する
-     * @returns {boolean}
-     */
-    validateRoute() {
-      const mode = this.getMode();
-      const key  = this.getKey();
-      const page = this.getPage();
-
-      if (mode === '' && key === '' && page === '') {
-        return true;
-      }
-
-      // -------------------------------------------------------
-      // 記事リスト
-      if (mode && key === '' && page === '') {
-        const modeNumber = this.toNumber(mode);
-
-        if (Validator.isInteger(modeNumber) && Validator.minNumber(modeNumber, 1)) {
-          return true;
-        }
-      }
-
-      // -------------------------------------------------------
-      // 記事詳細＆カテゴリリスト
-      if (mode && key && page === '') {
-        const keyNumber = this.toNumber(key);
-
-        if (mode === 'post' && Validator.isInteger(keyNumber) && Validator.minNumber(keyNumber, 1)) {
-          return true;
-        }
-
-        if (mode === 'category' && Validator.maxLength(key, 30)) {
-          return true;
-        }
-      }
-
-      // -------------------------------------------------------
-      // カテゴリリスト
-      if (mode && key && page) {
-        const pageNumber = this.toNumber(page);
-
-        console.log(mode, key, Validator.maxLength(key, 30), pageNumber);
-        if (mode === 'category' && Validator.maxLength(key, 30) && Validator.isInteger(pageNumber) && Validator.minNumber(pageNumber, 1)) {
-          return true;
-        }
-      }
-
-      return false;
-    },
   },
   mounted () {
-    if (!this.validateRoute()) {
+    if (!BlogUtil.validateRoute(this.getMode(), this.getKey(), this.getPage())) {
       this.$router.push({path: '/blog'});
     }
+    this.currentView = BlogUtil.mainComponentName(this.getMode());
   },
   components: {
     BlogSideBar,
