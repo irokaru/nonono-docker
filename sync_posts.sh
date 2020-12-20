7decb1323b6027a53e629def84f858e0dbc433c4
irokaru <karuta@nononotyaya.net>
2020-12-20 22:57:56 +0900

    fix shell

--- sync_posts.sh
+++ sync_posts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "start sync post data..."
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-rsync -auzv $SCRIPT_DIR/src/nonono/storage/app/posts nonono:/home/irokaru/nonono-docker/src/nonono/storage/app/public
+rsync -auzv $SCRIPT_DIR/src/nonono/storage/app/posts nonono:/home/irokaru/nonono-docker/src/nonono/storage/app
 echo "finished"
