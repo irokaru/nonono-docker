1d72429f929ffb13de880acd26a3bd218b66b1bb
irokaru <karuta@nononotyaya.net>
2020-12-20 09:47:55 +0900

    fix post model

--- src/nonono/resources/js/model/Post.js
+++ src/nonono/resources/js/model/Post.js
@@ -6,26 +6,32 @@ export default {
    */
   model: {
     id:           0,
-    title:        '',
+    title:        'ーーー',
     date:         DateUtil.formatDate('YYYY-MM-dd'),
     release_flag: true,
     detail:       '',
-    categories:   [],
+    categories:   [{post_id: 0, category: 'ーーー'}],
   },
 
   dummy(id) {
-    const model = this.model;
-    model.id    = id;
-    model.title = '－－－';
+    const model                 = Object.create(this.model);
+    model.id                    = id;
+    model.categories[0].post_id = id;
 
     return model;
+  },
+
+  dummysArray(len) {
+    return Array(len).fill().map((_, i) => {
+      return this.dummy((i + 1) * -1);
+    });
   },
 
   /**
    * カテゴリのモデル
    */
   category: {
-    category: '－－－',
+    category: 'ーーー',
     count:    '-',
   },
 };
