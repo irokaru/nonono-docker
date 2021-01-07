613abb03b1abb0d667171a940b8608e2486da5f7
irokaru <karuta@nononotyaya.net>
2021-01-07 18:56:55 +0900

    fix plugins

--- src/nonono/resources/js/plugins/Auth.js
+++ src/nonono/resources/js/plugins/Auth.js
@@ -1,32 +1,44 @@
-import bearer from '@websanova/vue-auth/drivers/auth/bearer'
-import axios from '@websanova/vue-auth/drivers/http/axios.1.x'
-import router from '@websanova/vue-auth/drivers/router/vue-router.2.x'
+import {createAuth} from '@websanova/vue-auth/src/v3.js';
+import bearer       from '@websanova/vue-auth/src/drivers/auth/bearer'
+import axios        from '@websanova/vue-auth/src/drivers/http/axios.1.x'
+import router       from '@websanova/vue-auth/src/drivers/router/vue-router.2.x'
+
 /**
  * Authentication configuration, some of the options can be override in method calls
  */
-const config = {
-  auth: bearer,
-  http: axios,
-  router: router,
-  tokenDefaultName: 'nonono-jwt-auth',
-  tokenStore: ['localStorage'],
-
-  // API endpoints used in Vue Auth.
-  loginData: {
-    url: '/api/auth/login',
-    method: 'POST',
-    redirect: '/admin',
-    fetchUser: true
-  },
-  logoutData: {
-    url: '/api/auth/logout',
-    method: 'POST',
-    redirect: '/',
-    makeRequest: true
-  },
-  refreshData: {
-    enabled: false,
-  },
-  fetchData: {enabled: false},
-}
-export default config
+export default (app) => {
+  app.use(createAuth({
+    plugins: {
+      http:   app.axios,
+      router: app.router,
+    },
+    drivers: {
+      auth:             bearer,
+      http:             axios,
+      router:           router,
+      tokenDefaultKey: 'nonono-jwt-auth',
+      stores:          ['localStorage'],
+    },
+    options: {
+      notFoundRedirect: {name: 'admin'},
+      loginData: {
+      url: '/api/auth/login',
+        method: 'POST',
+        redirect: '/admin',
+        fetchUser: true
+      },
+      logoutData: {
+        url: '/api/auth/logout',
+        method: 'POST',
+        redirect: '/',
+        makeRequest: true
+      },
+      refreshData: {
+        enabled: false,
+      },
+      fetchData: {
+        enabled: false
+      },
+    },
+  }));
+};
