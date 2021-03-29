19b54783972ef8f60f9caa3eabd8294bfc04b57e
irokaru <karuta@nononotyaya.net>
2021-03-29 21:00:07 +0900

    add validate patterns

--- src/nonono/tests/JavaScript/lib/BlogUtil.spec.js
+++ src/nonono/tests/JavaScript/lib/BlogUtil.spec.js
@@ -29,6 +29,43 @@ describe('validateRoute', () => {
       [true,  _r('1',   '', '')],
       [false, _r('0',   '', '')],
       [false, _r('-1',  '', '')],
+
+      // 記事詳細
+      [true,  _r('post', '100', '')],
+      [true,  _r('post', '1',   '')],
+      [false, _r('post', '0',   '')],
+      [false, _r('post', '-1',  '')],
+
+      // カテゴリリスト
+      [true, _r('category', 'aaaaa', '')],
+      [true, _r('category', '1',     '')],
+      [true, _r('category', '0',     '')],
+      [true, _r('category', '-1',    '')],
+
+      // カテゴリリスト(ページ付き)
+      [true,  _r('category', 'aaaaa', '1')],
+      [true,  _r('category', '1',     '100')],
+      [false, _r('category', '0',     '-1')],
+      [false, _r('category', '-1',    '0')],
+    ];
+
+    for (const suite of suites) {
+      const msg = JSON.stringify(suite);
+
+      expect(BlogUtil.validateRoute(suite[1]), msg).toBe(suite[0]);
+    }
+  });
+
+  test('[NG] invalid params', () => {
+    const suites = [
+      // expect, route
+      [false, _r('aaa', 'bbb', 'ccc')],
+
+      [false, _r('post', 'aaaaa', '')],
+      [false, _r('post', '111', '222')],
+
+      [false, _r('category', Array(31).fill('*').join(''), '')],
+      [false, _r('category', Array(31).fill('*').join(''), '1')],
     ];
 
     for (const suite of suites) {
